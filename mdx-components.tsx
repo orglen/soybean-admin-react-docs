db9be300d425bd89276849b36859c8bc52970a94
Ohh-889 <1509326266@qq.com>
2025-03-23 11:43:33 +0800

    feat: 完善mdx的组件

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,19 +1,42 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
+import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs';
 
-const {
-  tr: Tr,
-  th: Th,
-  table: Table,
-  img: Image,
-  ...docsComponents
-} = getDocsMDXComponents()
+const { img: Image, table: Table, th: Th, tr: Tr, ...docsComponents } = getDocsMDXComponents();
 
 export const useMDXComponents: typeof getDocsMDXComponents = components => ({
   ...docsComponents,
-  tr: Tr,
+  figcaption: props => (
+    <figcaption
+      className="mt-2 text-center text-sm"
+      {...props}
+    />
+  ),
+  figure: props => (
+    <figure
+      className="mt-6"
+      {...props}
+    />
+  ),
+  img: props => (
+    <Image
+      {...props}
+      className="nextra-border rounded-xl border drop-shadow-sm"
+    />
+  ),
+  table: props => (
+    <Table
+      className="w-full text-sm"
+      {...props}
+    />
+  ),
+  tbody: props => (
+    <tbody
+      className="break-words first:[&_td]:font-semibold first:[&_td]:text-violet-600 first:[&_td]:dark:text-violet-500 [&_tr]:!bg-transparent"
+      {...props}
+    />
+  ),
   th: Th,
   thead({ children, ...props }) {
     return (
@@ -24,31 +47,13 @@ export const useMDXComponents: typeof getDocsMDXComponents = components => ({
           <Tr>
             <Th align="left">Option</Th>
             <Th align="left">Type</Th>
-            {children.props.children.length === 4 && (
-              <Th align="left">Default Value</Th>
-            )}
+            {children.props.children.length === 4 && <Th align="left">Default Value</Th>}
             <Th align="left">Description</Th>
           </Tr>
         )}
       </thead>
-    )
+    );
   },
-  tbody: props => (
-    <tbody
-      className="break-words first:[&_td]:font-semibold first:[&_td]:text-violet-600 first:[&_td]:dark:text-violet-500 [&_tr]:!bg-transparent"
-      {...props}
-    />
-  ),
-  table: props => <Table className="w-full text-sm" {...props} />,
-  img: props => (
-    <Image
-      {...props}
-      className="nextra-border rounded-xl border drop-shadow-sm"
-    />
-  ),
-  figure: props => <figure className="mt-6" {...props} />,
-  figcaption: props => (
-    <figcaption className="mt-2 text-center text-sm" {...props} />
-  ),
+  tr: Tr,
   ...components
-})
+});
